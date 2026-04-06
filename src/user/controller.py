fced2713a27088b055a7d2c72854aa7c44b75aa9
Uday vara <varaudayd@gmail.com>
2026-04-06 13:51:45 +0530

    added login route

--- src/user/controller.py
+++ src/user/controller.py
@@ -1,8 +1,10 @@
 from tabnanny import check
 
+from fastapi import HTTPException
 import jwt
 from pwdlib import PasswordHash
 from sqlalchemy.orm import Session
+from src.user.dto.logindto import loginDTO
 from src.user.model import UserModel
 from src.user.dto.signupdto import SignupBody
 from src.utils.config import Settings
@@ -12,7 +14,7 @@ def signup(signup_data: SignupBody,db:Session):
     try:
         check_user = db.query(UserModel).filter((UserModel.name == signup_data.name) | (UserModel.email == signup_data.email)).first()
         if check_user:
-            return {"error": "User with this name or email already exists"}
+            raise HTTPException(status_code=400, detail="User with this name or email already exists")
 
         password_hash = PasswordHash.recommended()
 
@@ -26,7 +28,22 @@ def signup(signup_data: SignupBody,db:Session):
         db.commit()
         db.refresh(new_user)
         token = jwt.encode({"user_id": str(new_user.id)}, Settings.SECRET_KEY, algorithm=Settings.ALGORITHM)
-        return {"message": "User created successfully", "user_id": str(new_user.id)}
+        return {"message": "User created successfully", "token": token}
     except Exception as e:
-        db.rollback()
-        return {"error": str(e)}
+        return HTTPException(status_code=e.status_code if isinstance(e, HTTPException) else 500, detail=str(e))
+    
+
+def login(loginBody:loginDTO,db:Session):
+    try:
+        user = db.query(UserModel).filter(UserModel.email == loginBody.email).first()
+        if not user:
+            raise HTTPException(status_code=400, detail="Invalid email or password")
+        
+        password_hash = PasswordHash.recommended()
+        if not password_hash.verify(loginBody.password, user.password):
+            raise HTTPException(status_code=400, detail="Invalid email or password")
+        
+        token = jwt.encode({"user_id": str(user.id)}, Settings.SECRET_KEY, algorithm=Settings.ALGORITHM)
+        return {"message": "Login successful", "token": token}
+    except Exception as e:
+        raise HTTPException(status_code=e.status_code if isinstance(e, HTTPException) else 500, detail=str(e))
--- src/user/router.py
+++ src/user/router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from src.user.controller import signup
+from src.user.controller import login, signup
+from src.user.dto.logindto import loginDTO
 from src.user.dto.signupdto import SignupBody
 from src.utils.db import get_db
 
@@ -14,3 +15,7 @@ user_router = APIRouter(
 @user_router.post("/signup",status_code=201)
 def signupRoute(body: SignupBody,db:Session = Depends(get_db)):
     return signup(body,db)
+
+@user_router.post("/login")
+def loginRoute(body: loginDTO,db:Session = Depends(get_db)):
+    return login(body,db)
